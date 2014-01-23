141f35a86a881f91ca960e7c2112b51ea6119bf3
smadala <sattimadala@gmail.com>
2014-01-23 18:01:01 +0530

    first commit of dbs proJ

---
+++ plethora/src/DBSystem.java
@@ -0,0 +1,12 @@
+public class DBSystem {
+	public void readConfig(String configFilePath) {
+		
+	}
+
+	public void populatePageInfo() {
+	}
+
+	public String getRecord(String tableName, int recordId) {
+		return "record";
+	}
+}
