2c81e0a73c7ab35638ed1d069d62a6b73e1a40e0
Harshas <harshas1989@gmail.com>
2014-01-24 15:57:42 +0530

    TableList

--- plethora/src/DBSystem.java
+++ plethora/src/DBSystem.java
@@ -1,33 +1,44 @@
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.plethora.mem.ConfigConstants;
 import com.plethora.mem.DataBaseMemoryConfig;
-import com.plethora.obj.Page;
-import com.plethora.obj.Table;
+import com.plethora.obj.FileReader;
 
 public class DBSystem {
-	public Page mainMemoryPages[];
 	public void readConfig(String configFilePath) {
-		BufferedReader br=null;
+		InputStream br=null;
+		int flag=0;
+		List<String> tableNames=new ArrayList<String>();
 		try {
 			String line=null,tokens[]=null;
-			br=new BufferedReader(new FileReader(configFilePath));
-			
-			while( !ConfigConstants.TABLE_BEGIN.equals(line = br.readLine() )){
-				tokens=line.split(ConfigConstants.PROPS_DELIMITER);
-				//memoryProps.put(tokens[0],tokens[1]);
-				if(tokens[0].equals(ConfigConstants.PAGESIZE)){
-					DataBaseMemoryConfig.PAGE_SIZE=Integer.parseInt(tokens[1]);
+			br=new FileInputStream(configFilePath);
+			FileReader objectRead=new FileReader();
+			while( (line = objectRead.readLine(br))!=null ){
+				if((!(line.equals(ConfigConstants.TABLE_BEGIN)))&& flag!=1){
+					tokens=line.split(ConfigConstants.PROPS_DELIMITER);
+					//memoryProps.put(tokens[0],tokens[1]);
+					if(tokens[0].equals(ConfigConstants.PAGESIZE)){
+						DataBaseMemoryConfig.PAGE_SIZE=Integer.parseInt(tokens[1]);
+						}
+					else if(tokens[0].equals(ConfigConstants.NUM_PAGES)){
+						DataBaseMemoryConfig.NUM_OF_PAGES=Integer.parseInt(tokens[1]);
 					}
-				else if(tokens[0].equals(ConfigConstants.NUM_PAGES)){
-					DataBaseMemoryConfig.NUM_OF_PAGES=Integer.parseInt(tokens[1]);
+					else if(tokens[0].equals(ConfigConstants.PATH_FOR_DATA)){
+						DataBaseMemoryConfig.PATH_FOR_DATA=new String(tokens[1]);
+					}
 				}
-				else if(tokens[0].equals(ConfigConstants.PATH_FOR_DATA)){
-					DataBaseMemoryConfig.PATH_FOR_DATA=new String(tokens[1]);
+				else if(line.equals(ConfigConstants.TABLE_BEGIN)){
+					flag=1;
+				}
+				else if(flag==1){
+					tableNames.add(line);
+					flag=0;
 				}
 			}
 			/*Table table=new Table();
@@ -47,21 +58,20 @@ public class DBSystem {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
 	}
 
 	public void populatePageInfo() {
-		mainMemoryPages=new Page[DataBaseMemoryConfig.NUM_OF_PAGES];
+
 	}
 
 	public String getRecord(String tableName, int recordId) {
 		return "record";
 	}
-	/*public static void main(String args[]){
+	public static void main(String args[]){
 		DBSystem ob1=new DBSystem();
 		ob1.readConfig("/home/harshas/Desktop/config.txt");
 		System.out.println("# of Pages "+DataBaseMemoryConfig.NUM_OF_PAGES);
 		System.out.println("Page Size "+DataBaseMemoryConfig.PAGE_SIZE);
 		System.out.println("Path for Data "+DataBaseMemoryConfig.PATH_FOR_DATA);
-	}*/
+	}
 }
